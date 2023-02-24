b51ab8265f31ed82c934cd8aa3906e46d512f876
ShoheiTakino <shohei0522penguin@gmail.com>
2023-02-25 08:31:52 +0900

    Initial Commit

---
+++ ChouHan/ChouHanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChouHanApp.swift
+//  ChouHan
+//
+//  Created by 滝野翔平 on 2023/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ChouHanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
